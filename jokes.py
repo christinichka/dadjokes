fb3a296bf82ab99eab597851769edb4c3261a345
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2019-08-03 15:32:23 -0600

    Add files via upload

---
+++ jokes.py
@@ -0,0 +1,27 @@
+import requests
+from random import choice
+from pyfiglet import figlet_format
+from termcolor import colored
+
+header = figlet_format("Dad Jokes!")
+header = colored(header, color="green")
+print(header)
+
+user_input = input("What would you like to search for? ")
+url = "https://icanhazdadjoke.com/search"
+res = requests.get(
+	url, 
+	headers={"Accept": "application/json"},
+	params={"term":user_input}
+	).json()
+
+num_jokes = res["total_jokes"]
+results = res["results"]
+if num_jokes > 1:
+	print(f"I found {num_jokes} jokes about {user_input}. Here's one: ")
+	print(choice(results)["joke"])
+elif num_jokes == 1:
+	print("I found one joke about {user_input}")
+	print(results[0]['joke'])
+else:
+	print(f"Sorry, couldn't find a joke with your term: {user_input}")
